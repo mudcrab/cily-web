08f37056e10a1042a989cb2597cf0aed05585ae6
Jevgeni Krais <jevgeni@pitfire.eu>
2014-12-14 23:30:58 +0200

    builditem view

--- app/js/views/overview/builditem.js
+++ app/js/views/overview/builditem.js
@@ -6,17 +6,21 @@ Cily.View = Cily.View || {};
 
 		tagName: 'li',
 		className: 'item overview-project-builds--item',
+		template: Tpl.overview.builditem,
 
 		events: {},
 
 		initialize: function(options) 
 		{
-			// 
+			this.model = options.model || {};
+			this.model.datetime = this.model.end_time || this.model.start_time;
+			this.model.commit = this.model.commit || 'Failed';
 		},
 
 		render: function() 
 		{
-
+			this.$el.html(this.template(this.model));
+			return this;
 		}
 
 	});
