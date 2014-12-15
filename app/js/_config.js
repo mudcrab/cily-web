231bf513c91476fcb7f3d68e821b01dc641b8595
Jevgeni Krais <jevgeni@pitfire.eu>
2014-12-15 23:10:24 +0200

    set selectected option helper for handlebars

--- app/js/_config.js
+++ app/js/_config.js
@@ -9,3 +9,9 @@ var _setHeaders = function(xhr)
 	xhr.setRequestHeader('X-Cily-UID', $.cookie('uid'));
 	xhr.setRequestHeader('X-Cily-Token', $.cookie('token'));
 };
+
+window.Handlebars.registerHelper('select', function( value, options ){
+	var $el = $('<select />').html( options.fn(this) );
+	$el.find('[value=' + value + ']').attr({'selected':'selected'});
+	return $el.html();
+});
